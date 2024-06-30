211fef2cb4de2e4fa57f94c2eb5c0979322d860f
Alex_Sousa <146445764+AlexSousaVeras@users.noreply.github.com>
2024-06-30 11:48:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function calcular(){
+    var m = document.getElementById('mediaEs').value;
+
+    let n1 = document.getElementById('nota1').value;
+    let n2 = document.getElementById('nota2').value;
+    let n3 = document.getElementById('nota3').value;
+
+    var M = parseFloat(m);
+
+    let x = parseFloat(n1);
+    let y = parseFloat(n2);
+    let z = parseFloat(n3);
+        
+    let media = x + y + z;
+    let media1 = media/3;
+    let diferença = M - media1;
+    
+    if (media1 >= M){
+        document.getElementById('resultado').innerText = 'Sua media foi: ' + media1 +', meus parabéns você está na media!';
+        return false;
+    }
+
+    else {
+        document.getElementById('resultado').innerText = 'Sua media foi: ' + media1 +', você precia de ' + diferença + ' pontos para está na media!';
+        return false;
+    }
+
+    }
